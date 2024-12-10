3086eefbcb29b8478be11381820660a432b57ac9
Fanjaaa_ <irfannurroja28@gmail.com>
2024-12-11 01:08:52 +0700

    Responsive Update

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
 import { Context } from '../../context/Context'
@@ -10,10 +10,31 @@ const Sidebar = () => {
     const [showCard, setShowCard] = useState(false)
     const {onSent, prevPrompts, setRecentPrompt, newChat} = useContext(Context)
 
+    const cardRef = useRef(null); // Referensi untuk elemen card-item
+
     const loadPrompt = async (prompt) => {
         setRecentPrompt(prompt)
         onSent(prompt)
     }
+
+      // Fungsi untuk mendeteksi klik di luar elemen card-item
+    const handleOutsideClick = (e) => {
+      if (cardRef.current && !cardRef.current.contains(e.target)) {
+        setShowCard(false); // Sembunyikan card-item jika klik di luar
+      }
+    };
+
+    useEffect(() => {
+      if (showCard) {
+        document.addEventListener('mousedown', handleOutsideClick);
+      } else {
+        document.removeEventListener('mousedown', handleOutsideClick);
+      }
+
+      return () => {
+        document.removeEventListener('mousedown', handleOutsideClick);
+      };
+    }, [showCard]);
 
   return (
     <div className={`sidebar ${extended ? "extended" : ""}`}>
@@ -58,7 +79,7 @@ const Sidebar = () => {
         </div>
       </div>
       {showCard? 
-        <div className="card-item">
+        <div ref={cardRef} className="card-item">
           &quot;App developed by <a href='https://www.linkedin.com/in/fanjaaa/' target='blank' rel='noopener noreferrer'>Fanjaa</a>. Special thanks to the <a href='https://ai.google.dev/' target='blank' rel='noopener noreferrer'>Gemini AI</a> team for providing the powerful API that makes this app possible.&quot;
         </div>
       :null}
